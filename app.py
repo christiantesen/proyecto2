2a09c83f72979a33adbb7f49de154d1183460f65
Hyre <developer.hyre@gmail.com>
2024-12-06 10:55:23 -0500

    .

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from fasthtml.common import FastHTML, serve
+from flask import Flask, render_template, send_from_directory,url_for
 from jinja2 import Environment, FileSystemLoader
 from limpieza import leer_datos_csv
+import os
 
 # Inicializar la aplicación
-app = FastHTML()
-env = Environment(loader=FileSystemLoader('.'))
+app = Flask(__name__)
+env = Environment(loader=FileSystemLoader('templates'))
+env.globals['url_for'] = url_for  # Registrar url_for manualmente
+
 # Función para convertir el DataFrame a HTML
 def generar_tabla_html(df):
     filas = ""
@@ -18,17 +22,19 @@ def generar_tabla_html(df):
 def home():
     # Ruta del archivo CSV que quieres leer
     ruta_archivo = 'Enaho01-2020-100.csv'
+    if not os.path.exists(ruta_archivo):
+        raise FileNotFoundError(f"El archivo {ruta_archivo} no existe.")
     df = leer_datos_csv(ruta_archivo)  # Leer los datos desde el archivo CSV
-
-    # Generar la tabla HTML a partir de los datos
-    columns_list = list(df.columns)
-  
     # Crear la página HTML con los estilos de tabla y colo pasteles
-    
     list_of_dicts = df.to_dict(orient='records')   
-
     template = env.get_template('index.html')
     return  template.render({ "registros":list_of_dicts,"columns": list(df.columns)})
 
-# Servir la aplicación
-serve()
+# Nueva ruta para la página de gráficos
+@app.get('/graficos')
+def graficos():
+    template = env.get_template('graficos.html')
+    return template.render()
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- limpieza.py
+++ limpieza.py
@@ -1,21 +1,24 @@
 import pandas as pd
+import numpy as np
 
 # Función para leer los datos desde un archivo CSV
 def leer_datos_csv(ruta_archivo):
     df = pd.read_csv(ruta_archivo, encoding='latin-1')
-    df_final = df[['DOMINIO', 'P101', 'P102', 'P103', 'P103A', 'P110F', 'P112A', 'P110C', 'P110C2', 'P110C3']]
+    df_final = df[['P101', 'P102', 'P103', 'P103A', 'P110F', 'P112A', 'P110C', 'P110C2', 'P110C3', 'P110', 'P105A']]
     df_final.head() 
     df_final.replace("", pd.NA, inplace=True)  
     df_final.replace(" ", pd.NA, inplace=True) 
     df_final.drop(columns=['P110C2', 'P110C3'], inplace=True)
     df_final.rename(columns={
-                         'P101':'TIPO_DE_VIVIENDA',
-                         'P102':'MATERIAL_PARED_EXTERIOR',
-                         'P103':'MATERIAL_PISO',
-                         'P103A':'MATERIAL_TECHO',
-                         'P110F':'PAGO_SERVICIO_AGUA',
-                         'P112A':'ELECTRICO_EXCLUSIVO_COLECTIVO',
-                         'P110C':'AGUA_SEMANA'}, inplace=True)
+                            'P101': 'TIPO_DE_VIVIENDA',
+                            'P102': 'MATERIAL_PARED_EXTERIOR',
+                            'P103': 'MATERIAL_PISO',
+                            'P103A': 'MATERIAL_TECHO',
+                            'P110F': 'PAGO_SERVICIO_AGUA',
+                            'P112A': 'ELECTRICO_EXCLUSIVO_COLECTIVO',
+                            'P110C': 'AGUA_SEMANA',
+                            'P105A': 'TIPO_POSESION_VIVIENDA',
+                            'P110': 'PROCEDENCIA_DE_AGUA'}, inplace=True)
     mode_values = df_final.mode().iloc[0]
     df_final.fillna(mode_values, inplace=True)
     df_final.head()
